70434e567ef3f6b1bf72f8b37e30e7283a5162ba
Brian Edwards <brian@edwardstx.us>
2018-05-25 21:20:02 -0700

    Use servive token

--- src/clj/io/hosaka/auth/keys.clj
+++ src/clj/io/hosaka/auth/keys.clj
@@ -4,30 +4,45 @@
             [com.stuartsierra.component :as component]
             [clj-time.core :as time]))
 
-(defrecord Keys [key env]
+(defrecord Keys [key env token]
   component/Lifecycle
 
   (start [this]
     (assoc
      this
-     :key
-     (crypto/decode-private-key {:algorithm "ECDSA"
+     :token (atom nil)
+     :key (crypto/decode-private-key {:algorithm "ECDSA"
                                  :bytes (crypto/decode-base64 (:service-key env))})))
 
   (stop [this]
-    (assoc this :key nil)))
+    (assoc this :key nil :token nil)))
 
 (defn new-keys [env]
   (map->Keys {:env (select-keys env [:service-key :service-id])}))
 
-(defn create-token [{:keys [env key]} claims]
-  (let [n (time/now)]
-    (jwt/sign
-     (assoc claims
-            :iss (:service-id env)
-            :iat n
-            :jti (str (java.util.UUID/randomUUID))
-            :exp (time/plus n (time/hours 8)))
-     key
-     {:alg :es256 :header {:kid (:service-id env)}})))
+(defn create-token
+  ([keys claims] (create-token keys claims (time/now)))
+  ([{:keys [env key]} claims n]
+   (jwt/sign
+    (assoc claims
+           :iss (:service-id env)
+           :iat n
+           :jti (str (java.util.UUID/randomUUID))
+           :exp (time/plus n (time/hours 8)))
+    key
+    {:alg :es256 :header {:kid (:service-id env)}})))
+
+(defn get-service-token [keys]
+  (:token
+   (swap!
+    (:token keys)
+    (fn [token]
+      (let [n (clj-time.core/now)]
+        (if (or
+             (nil? token)
+             (clj-time.core/after? n (:exp token)))
+          (hash-map
+           :token (create-token keys {:sub (-> keys :env :service-id)} n)
+           :exp (time/plus n (time/hours 7)))
+          token))))))
 
